8a6cd7909d32384e5efd077be622cfbc12e0c374
Khoa Dang <hoanganhkhoadang@gmail.com>
2026-04-09 15:16:44 +0700

    fixe bugs

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, send_from_directory
 import os
+import matplotlib
+matplotlib.use('Agg')
 
 app = Flask(__name__)
+
+if not os.path.exists("co2_temp_graph.png"):
+    open("co2_temp_graph.png","a").close()
+
+if not os.path.exists("temp_graph.png"):
+    open("temp_graph.png","a").close()
+
+if not os.path.exists("results.json"):
+    with open("results.json","w") as f:
+        f.write("{}")
 
 @app.route("/")
 def home():
